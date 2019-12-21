49d3e030838a3f2f213811a9179b7d8c017fde04
FColor04 <Marcinure1@gmail.com>
2019-12-21 22:32:44 +0100

    Update startup.js

--- autoupdater/scripts/startup.js
+++ autoupdater/scripts/startup.js
@@ -1,1 +1,5 @@
 console.log("Startup!");
+document.getElementById("version").innerHTML = "0.1.1v";
+let startMinecraft = function () {
+	exec('start '+process.cwd()+'/minecraft/startMinecraft.bat');
+}
