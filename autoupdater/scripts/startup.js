dc055054a64525cdfb3b4d4ddfeb97047dd23950
FColor04 <Marcinure1@gmail.com>
2019-12-22 14:01:36 +0100

    Mods update

--- autoupdater/scripts/startup.js
+++ autoupdater/scripts/startup.js
@@ -1,5 +1,5 @@
 console.log("Startup!");
-global.sharedObj = {version: null, startMinecraft: null, resetConfig: null};
+global.sharedObj = {version: null, startMinecraft: null, resetConfig: null, redownloadMods: null};
 global.sharedObj.version = "0.0.1r";
 global.sharedObj.resetConfig = function () {
 	deleteFolderRecursive("./minecraft/.minecraft/config/Embassy");
@@ -8,4 +8,13 @@ global.sharedObj.startMinecraft = function () {
 	global.sharedObj.resetConfig();
 	exec('start '+process.cwd()+'/startMinecraft.bat');
 	process.exit(0);
+}
+global.sharedObj.redownloadMods = function () {
+	let modsArray = remethorSettings.mods;
+    deleteFolderRecursive('./miencraft/.minecraft/mods');
+    mkDir("./minecraft/.minecraft/mods");
+    for(let i=0;i<modsArray.length;i++){
+    	downloadFile('https://remethor.github.io/autoupdater/mods/'+modsArray[i], './minecraft/.minecraft/mods/'+modsArray[i]);
+    }
+    alert("Proszę chwilę zaczekać!");
 }
