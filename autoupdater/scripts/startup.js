e4eba47ce772b453273461782d18f3579831e040
FColor04 <Marcinure1@gmail.com>
2019-12-22 13:31:36 +0100

    Update startup.js

--- autoupdater/scripts/startup.js
+++ autoupdater/scripts/startup.js
@@ -1,7 +1,11 @@
 console.log("Startup!");
-global.sharedObj = {version: null, startMinecraft: null};
+global.sharedObj = {version: null, startMinecraft: null, resetConfig: null};
 global.sharedObj.version = "0.0.1r";
+global.sharedObj.resetConfig = function () {
+	deleteFolderRecursive("./minecraft/.minecraft/config/Embassy");
+}
 global.sharedObj.startMinecraft = function () {
+	global.sharedObj.resetConfig();
 	exec('start '+process.cwd()+'/startMinecraft.bat');
 	process.exit(0);
 }
