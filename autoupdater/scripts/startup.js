1a3a272226897a525ad1992b12f23926d6370731
FColor04 <Marcinure1@gmail.com>
2019-12-22 16:51:44 +0100

    Script update

--- autoupdater/scripts/startup.js
+++ autoupdater/scripts/startup.js
@@ -1,15 +1,16 @@
 console.log("Startup!");
 global.sharedObj = {version: null, startMinecraft: null, resetConfig: null, redownloadMods: null};
-global.sharedObj.version = "0.0.1r";
+global.sharedObj.version = "0.1.2r";
 global.sharedObj.resetConfig = function () {
 	deleteFolderRecursive("./minecraft/.minecraft/config/Embassy");
-	deleteFolderRecursive("./minecraft/.minecraft/config/betterhud.cfg");
+	fs.unlinkSync("./minecraft/.minecraft/config/betterhud.cfg");
+	console.log("Pulling config");
 	downloadFile('https://remethor.github.io/autoupdater/config/betterhud.cfg', './minecraft/.minecraft/config/betterhud.cfg', function(){
-		console.log("done");
+		console.log("Done!");
 	});
 }
+global.sharedObj.resetConfig();
 global.sharedObj.startMinecraft = function () {
-	global.sharedObj.resetConfig();
 	exec('start '+process.cwd()+'/startMinecraft.bat');
 	process.exit(0);
 }
